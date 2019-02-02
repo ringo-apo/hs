1e689c9f1241e424cda2f2ba8f5a528362666330
apo <apo@localhost.localdomain>
2019-02-03 07:36:34 +0900

    201902030736

--- my-project/app/Main.hs
+++ my-project/app/Main.hs
@@ -4,6 +4,8 @@ import Lib
 
 main :: IO ()
 main = do
-    print "Please input your name."
-    name <- getLine
-    print ("Hello, " ++ name ++ " san.")
+    print "Please input any number."
+    nums <- getLine
+    let num = read nums :: Int
+    let num2 = num + 1
+    print (num2)
